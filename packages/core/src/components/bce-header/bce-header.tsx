cb0f68d4bc3be147a9ed56417469fb1ef529d243
RCVZ <robinvz17@gmail.com>
2019-04-18 09:41:25 +0200

    chore: bce-menu

--- packages/core/src/components/bce-header/bce-header.tsx
+++ packages/core/src/components/bce-header/bce-header.tsx
@@ -1,16 +1,24 @@
-import { Component, Prop } from '@stencil/core';
+import { Component, Prop } from "@stencil/core";
 
-import { Color } from '../../models/color';
+import { Color } from "../../models/color";
 
 @Component({
-  tag: 'bce-header',
-  styleUrl: 'bce-header.scss'
+  tag: "bce-header",
+  styleUrl: "bce-header.scss"
 })
 export class BceHeader {
   @Prop({ reflectToAttr: true })
   public color?: Color;
 
+  @Prop()
+  public logoSrc?: string;
+
   render() {
-    return <slot />;
+    return [
+      <img src={this.logoSrc} />,
+      <bce-menu>
+        <slot />
+      </bce-menu>
+    ];
   }
 }
