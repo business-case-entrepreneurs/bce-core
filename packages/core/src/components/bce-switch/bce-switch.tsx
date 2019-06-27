41a5faf89144f4c328619af39584c0ed878b752e
Rick Koster <ngf.koster@outlook.com>
2019-06-27 14:02:41 +0200

    feat: bce-switch focus attribute & styling

--- packages/core/src/components/bce-switch/bce-switch.tsx
+++ packages/core/src/components/bce-switch/bce-switch.tsx
@@ -1,5 +1,4 @@
-import { Component, Element, Prop, h } from '@stencil/core';
-
+import { Component, Element, h, Prop, Host } from '@stencil/core';
 import { Color } from '../../models/color';
 
 @Component({
@@ -19,6 +18,9 @@ export class BceSwitch {
   @Prop({ reflectToAttr: true })
   public disabled = false;
 
+  @Prop({ attr: 'focus', reflectToAttr: true, mutable: true })
+  public hasFocus = false;
+
   private handleChange = (event: Event) => {
     const input = event.target as HTMLInputElement | undefined;
     if (input) this.value = input.checked;
@@ -29,18 +31,33 @@ export class BceSwitch {
     event.cancelBubble = true;
   };
 
+  private handleFocus = () => {
+    this.hasFocus = true;
+  };
+
+  private handleBlur = () => {
+    this.hasFocus = false;
+  };
+
   render() {
     return (
-      <label>
-        <input
-          type="checkbox"
-          checked={this.value}
-          disabled={this.disabled}
-          onChange={this.handleChange}
-          onInput={this.handleInput}
-        />
-        <div data-on={this.value} data-off={!this.value} />
-      </label>
+      <Host
+        tabIndex={this.disabled ? undefined : 0}
+        onFocus={this.handleFocus}
+        onBlur={this.handleBlur}
+      >
+        <label>
+          <input
+            type="checkbox"
+            tabIndex={-1}
+            checked={this.value}
+            disabled={this.disabled}
+            onChange={this.handleChange}
+            onInput={this.handleInput}
+          />
+          <div data-on={this.value} data-off={!this.value} />
+        </label>
+      </Host>
     );
   }
 }
