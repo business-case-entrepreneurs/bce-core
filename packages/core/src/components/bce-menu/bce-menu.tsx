13c6dbf55193bffb0cae8dc3eba7381733c05471
RCVZ <robinvz17@gmail.com>
2019-07-15 20:52:08 +0200

    feat: navigations

--- packages/core/src/components/bce-menu/bce-menu.tsx
+++ packages/core/src/components/bce-menu/bce-menu.tsx
@@ -1,5 +1,12 @@
-import { Component, Prop, h } from '@stencil/core';
-
+import {
+  Component,
+  Prop,
+  Host,
+  Element,
+  State,
+  Listen,
+  h
+} from '@stencil/core';
 import { Color } from '../../models/color';
 
 @Component({
@@ -7,11 +14,81 @@ import { Color } from '../../models/color';
   styleUrl: 'bce-menu.scss'
 })
 export class BceMenu {
+  @Prop()
+  public verticale: boolean = false;
+
+  @Prop({ reflect: true })
+  public color?: Color;
+
+  @Element() el!: HTMLElement;
+
+  @State() isMobileMenuShown: boolean = false;
+
+  @Listen('resize', { target: 'window' })
+  public handleResize() {
+    requestAnimationFrame(() => {
+      if (window.innerWidth > 768) {
+        const menu = this.el.querySelector('.menu') as HTMLElement;
+        menu.style.display = '';
+        this.el.classList.remove('show-mobile-menu');
+        document.body.classList.remove('no-scroll');
+        this.isMobileMenuShown = false;
+      }
+    });
+  }
+
+  public componentDidLoad() {
+    this.isMobileMenuShown === false;
+  }
+
+  public showNav = () => {
+    if (this.isMobileMenuShown) return;
+    this.isMobileMenuShown = true;
+
+    const menu = this.el.querySelector('.menu') as HTMLElement;
+
+    menu.style.display = 'flex';
+    setTimeout(() => {
+      this.el.classList.add('show-mobile-menu');
+      document.body.classList.add('no-scroll');
+    }, 1);
+  };
+
+  public hideNav = () => {
+    if (!this.isMobileMenuShown) return;
+    this.isMobileMenuShown = false;
+
+    const menu = this.el.querySelector('.menu') as HTMLElement;
+
+    this.el.classList.remove('show-mobile-menu');
+
+    menu.style.display = 'none';
+    setTimeout(() => {
+      document.body.classList.remove('no-scroll');
+    }, 300);
+  };
+
+  public onClick = () => {
+    if (this.isMobileMenuShown) {
+      this.hideNav();
+    } else {
+      this.showNav();
+    }
+  };
+
   render() {
     return (
-      <nav>
-        <slot />
-      </nav>
+      <Host class={{ verticale: this.verticale }}>
+        <bce-icon
+          onClick={this.onClick}
+          class="mobile-button"
+          size={'lg'}
+          fixed-width
+        />
+        <div class="menu show-mobile-menu">
+          <slot />
+        </div>
+      </Host>
     );
   }
 }
