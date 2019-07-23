a2cad444320b17e02354c5c8cc39bc029299d12c
RCVZ <robinvz17@gmail.com>
2019-07-23 13:49:52 +0200

    feat: bce dropdown

--- packages/core/src/components/bce-dropdown-menu/bce-dropdown-menu.tsx
+++ packages/core/src/components/bce-dropdown-menu/bce-dropdown-menu.tsx
@@ -1,5 +1,5 @@
-import { Component, Element, Prop, h, Host } from '@stencil/core';
-import { ButtonType } from '../../models/button-type';
+import { Component, Element, Prop, h, Host, Listen } from '@stencil/core';
+import Popper from 'popper.js';
 
 @Component({
   tag: 'bce-dropdown-menu',
@@ -7,12 +7,52 @@ import { ButtonType } from '../../models/button-type';
   shadow: false
 })
 export class BceDropdownMenu {
+  public dropDownMenu!: Popper;
+  @Prop({ reflect: true, mutable: true })
+  public active: boolean = false;
+
+  @Element()
+  private el!: HTMLElement;
+
+  @Listen('click', { target: 'window' })
+  public closeDropdown() {
+    this.active = false;
+  }
+
+  componentDidLoad() {
+    const reference = this.el.querySelector('.dropdown-button')!;
+    const dropdown = this.el.querySelector('.dropdown-menu')!;
+
+    this.dropDownMenu = new Popper(reference, dropdown, {
+      placement: 'bottom-end',
+      eventsEnabled: true
+    });
+  }
+
+  componentDidUnload() {
+    this.dropDownMenu.destroy();
+  }
+
+  handleClick(event: Event) {
+    this.active = !this.active;
+    event.stopPropagation();
+  }
+
   render() {
-    return [
-      <bce-icon raw={'ellipsis-h'} size="lg" pre={'fas'} fixed-width />,
-      <div class="dropdown">
-        <slot />
-      </div>
-    ];
+    return (
+      <Host onClick={this.handleClick}>
+        <bce-icon
+          class="dropdown-button"
+          raw={'ellipsis-h'}
+          size="lg"
+          pre={'fas'}
+          fixed-width
+        />
+
+        <div class="dropdown-menu" data-active={this.active}>
+          <slot />
+        </div>
+      </Host>
+    );
   }
 }
