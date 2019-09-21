f9d222d75ef7c6d1718963dd3aab442e0a73a241
Rick Koster <ngf.koster@outlook.com>
2019-09-21 15:23:07 +0200

    chore: remove stencil www task & gh pages deployment

--- packages/core/stencil.config.ts
+++ packages/core/stencil.config.ts
@@ -4,13 +4,7 @@ import { sass } from '@stencil/sass';
 export const config: Config = {
   namespace: 'bce',
   globalStyle: './src/index.scss',
-  devServer: {
-    openBrowser: false
-  },
   plugins: [sass({ injectGlobalPaths: ['./scss/index.scss'] })],
-  outputTargets: [
-    { type: 'dist', dir: './dist', esmLoaderPath: '../loader' },
-    { type: 'www', dir: './www', serviceWorker: null }
-  ],
+  outputTargets: [{ type: 'dist', dir: './dist', esmLoaderPath: '../loader' }],
   copy: [{ src: 'logo' }]
 };
