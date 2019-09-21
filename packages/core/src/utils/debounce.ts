0af66e76e05dad4648e59f4b135a8e8cc41b0084
Rick Koster <ngf.koster@outlook.com>
2019-09-21 15:21:52 +0200

    feat: prepare core set for docs project

--- packages/core/src/utils/debounce.ts
+++ packages/core/src/utils/debounce.ts
@@ -1,4 +1,4 @@
-export function debounce<T extends (...args: any) => any>(func: T, wait = 200) {
+export function debounce<T extends (...args: any[]) => any>(f: T, wait = 200) {
   let timeout: number | undefined = undefined;
 
   return function(this: any) {
@@ -6,6 +6,6 @@ export function debounce<T extends (...args: any) => any>(func: T, wait = 200) {
     const args = arguments as any;
 
     window.clearTimeout(timeout);
-    timeout = window.setTimeout(() => func.apply(context, args), wait);
+    timeout = window.setTimeout(() => f.apply(context, args), wait);
   } as (...args: Parameters<T>) => void;
 }
