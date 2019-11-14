dd5e64feddb5e0c894c4183c32f1746e5d94f05b
RCVZ <robinvz17@gmail.com>
2019-11-14 12:22:31 +0100

    feat: vertical menu

--- packages/core/src/components/bce-menu/bce-menu.tsx
+++ packages/core/src/components/bce-menu/bce-menu.tsx
@@ -64,7 +64,7 @@ export class BceMenu {
           class={{
             menu: true,
             horizontal: !this.vertical,
-            verticle: this.vertical
+            vertical: this.vertical
           }}
         >
           <slot />
