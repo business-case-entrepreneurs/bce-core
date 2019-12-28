923ce824a06861a996b0cb931c8488677320527f
Rick Koster <ngf.koster@outlook.com>
2019-12-28 14:53:50 +0100

    feat: add bce-code component

---
+++ packages/core/src/components/code/code.tsx
@@ -0,0 +1,67 @@
+import 'prismjs';
+import 'prismjs/components/prism-jsx.min';
+import 'prismjs/components/prism-tsx.min';
+import 'prismjs/components/prism-typescript.min';
+
+import { Component, Element, h, Prop } from '@stencil/core';
+
+// Set Prism to manual mode
+(window.Prism as any).manual = true;
+
+export type BceCodeLanguage =
+  | 'clike'
+  | 'css'
+  | 'html'
+  | 'javascript'
+  | 'js'
+  | 'jsx'
+  | 'mathml'
+  | 'none'
+  | 'svg'
+  | 'ts'
+  | 'tsx'
+  | 'typescript'
+  | 'xml';
+
+@Component({
+  tag: 'bce-code',
+  styleUrl: 'code.scss',
+  shadow: true
+})
+export class Code {
+  @Element()
+  private el!: HTMLBceCodeElement;
+
+  @Prop({ reflect: true })
+  public language: BceCodeLanguage = 'none';
+
+  @Prop()
+  public content: string | string[] = '';
+
+  @Prop({ reflect: true })
+  public inline?: boolean;
+
+  @Prop({ reflect: true })
+  public async?: boolean;
+
+  componentDidLoad() {
+    const query = this.inline ? 'code' : 'pre';
+    const el = this.el.shadowRoot!.querySelector(query)!;
+    if (el) window.Prism.highlightAllUnder(el, !!this.async);
+  }
+
+  render() {
+    const classes = `language-${this.language}`;
+    const content = Array.isArray(this.content)
+      ? this.content.join('\n')
+      : this.content;
+
+    return this.inline ? (
+      <code class={classes}>{content}</code>
+    ) : (
+      <pre class={classes}>
+        <code>{content}</code>
+      </pre>
+    );
+  }
+}
