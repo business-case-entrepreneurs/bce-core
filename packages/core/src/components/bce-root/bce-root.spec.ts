0861232c085c290ea7ec1cd9dd2c8d551464b8fe
Robin van Zanten <38441984+RCVZ@users.noreply.github.com>
2020-03-04 16:44:56 +0100

    feat: tests bce-root

--- packages/core/src/components/bce-root/bce-root.spec.ts
+++ packages/core/src/components/bce-root/bce-root.spec.ts
@@ -1,33 +1,69 @@
-import { newSpecPage } from '@stencil/core/testing';
+jest.mock('../../utils/uuid');
+
+import { newSpecPage, SpecPage } from '@stencil/core/testing';
 import { Root } from './bce-root';
 
-jest.mock('../../utils/uuid', () => {
-  class UUID {
-    public static v4(): string {
-      return 'uidsgargrwthrt';
-    }
-  }
-  return {
-    UUID: UUID.v4()
-  };
-});
+describe('Testing bce-root', () => {
+  let page: SpecPage;
 
-describe('testing bce-root', () => {
   it('builds', () => {
     expect(new Root()).toBeTruthy();
   });
 
-  it('should render bce-root', async () => {
-    const page = await newSpecPage({
+  beforeEach(async () => {
+    page = await newSpecPage({
       components: [Root],
-      html: `<bce-root></bce-root>`
+      html: `<bce-root id="bce-root"></bce-root>`
     });
-
-    expect(page.root).toEqualHtml(`
-    <bce-root id="agjkhgiureagerhgqh">
-      <mock:shadow-root>
-        <slot></slot>
-      </mock:shadow-root>
-    </bce-root>`);
   });
+
+  it('render bce-root with bce-root as id', async () => {
+    expect(page.root).toEqualHtml(`
+      <bce-root id="bce-root">
+        <mock:shadow-root>
+          <slot></slot>
+        </mock:shadow-root>
+      </bce-root>`);
+  });
+
+  it('render bce-root bce-root with unique id', async () => {
+    expect(page.root).toMatchSnapshot();
+    expect(page.root!.id).toBeTruthy();
+  });
+
+  it('bce-root should render message', async () => {
+    const bceRoot = page.root!;
+    await bceRoot.message('test', { duration: 5, color: 'dark' });
+
+    await page.waitForChanges();
+
+    expect(page.root).toMatchSnapshot();
+  });
+
+  it('bce-root method execute resolves', async () => {
+    const bceRoot = page.root!;
+    let component = page.doc.createElement('test');
+    bceRoot.appendChild(component);
+
+    const fnTrue = function(res: any) {
+      return res();
+    };
+
+    await bceRoot.execute(component, fnTrue);
+    expect(bceRoot).resolves;
+  });
+
+  ///  todo check with rick
+  // it('bce-root method execute rejects', async () => {
+  //   const bceRoot = page.root!;
+  //   let component = page.doc.createElement('test');
+  //   bceRoot.appendChild(component);
+
+  //   const fnFalse = function(_: any, rej: any) {
+  //     return rej();
+  //   };
+
+  //   await bceRoot.execute(component, fnFalse);
+  //   expect(bceRoot).toBeUndefined;
+  // });
 });
