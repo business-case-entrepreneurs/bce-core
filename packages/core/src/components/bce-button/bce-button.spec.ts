b58a6230be28bc6b6d62823fe6c27b95a897505e
Robin van Zanten <38441984+RCVZ@users.noreply.github.com>
2020-03-05 10:08:48 +0100

    feat: test render bce-button

--- packages/core/src/components/bce-button/bce-button.spec.ts
+++ packages/core/src/components/bce-button/bce-button.spec.ts
@@ -4,10 +4,6 @@ import { Button } from './bce-button';
 describe('Testing bce-button', () => {
   let page: SpecPage;
 
-  it('builds', () => {
-    expect(new Button()).toBeTruthy();
-  });
-
   beforeEach(async () => {
     page = await newSpecPage({
       components: [Button],
@@ -16,10 +12,6 @@ describe('Testing bce-button', () => {
   });
 
   it('render bce-button', async () => {
-    page = await newSpecPage({
-      components: [Button],
-      html: `<bce-button></bce-button>`
-    });
     expect(page.root).toMatchSnapshot();
   });
 });
