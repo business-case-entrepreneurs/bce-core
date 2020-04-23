d7b4508be5f2f8d3db504f7f5215e5db1fcafe93
NGF Koster <ngf.koster@gmail.com>
2020-04-23 13:30:01 +0200

    chore: update prettier

--- packages/core/src/utils/debounce.ts
+++ packages/core/src/utils/debounce.ts
@@ -1,7 +1,7 @@
 export function debounce<T extends (...args: any[]) => any>(f: T, wait = 200) {
   let timeout: number | undefined = undefined;
 
-  return function(this: any) {
+  return function (this: any) {
     const context = this;
     const args = arguments as any;
 
