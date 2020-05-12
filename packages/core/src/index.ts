13e706336a1c7fe73bd3dc73d7baa39d7d4a1fa2
NGF Koster <ngf.koster@gmail.com>
2020-05-12 15:52:40 +0200

    feat: export file-manager

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,6 +4,7 @@ export * from './models/file-ref';
 export * from './models/file-server';
 export * from './utils/bce-file';
 export * from './utils/debounce';
+export * from './utils/file-manager';
 export * from './utils/is-email';
 export * from './utils/ripple';
 export * from './utils/scroll-spy';
