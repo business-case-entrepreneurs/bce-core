57afd4cc3cee47ba1903505143d76cc1c8585787
NGF Koster <ngf.koster@gmail.com>
2020-06-05 13:09:25 +0200

    feat: add ability to ensure last execution on throttle

--- packages/core/src/utils/throttle.ts
+++ packages/core/src/utils/throttle.ts
@@ -1,13 +1,29 @@
-export function throttle<T extends (...args: any[]) => any>(f: T, wait = 200) {
+interface ThrottleOptions {
+  ensureLast: boolean;
+}
+
+export function throttle<T extends (...args: any[]) => any>(
+  f: T,
+  wait = 200,
+  options: Partial<ThrottleOptions> = {}
+) {
+  const opts: ThrottleOptions = { ensureLast: false, ...options };
+
   let throttled = false;
+  let context: any = null;
+  let args: any = [];
 
   return function (this: any) {
+    context = this;
+    args = arguments;
     if (throttled) return;
-    const context = this;
-    const args = arguments as any;
 
     f.apply(context, args);
     throttled = true;
-    setTimeout(() => (throttled = false), wait);
+
+    setTimeout(() => {
+      throttled = false;
+      if (opts.ensureLast) f.apply(context, args);
+    }, wait);
   } as (...args: Parameters<T>) => void;
 }
